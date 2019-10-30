156944b06334c2f6f7aa53fd6773bacb1d4d952e
fpawel <binf1611@gmail.com>
2019-10-30 15:39:42 +0300

    remove logfile mutex and simplify logfile

--- pkg/logfile/output.go
+++ pkg/logfile/output.go
@@ -3,10 +3,8 @@ package logfile
 import (
 	"bytes"
 	"fmt"
-	"github.com/powerman/structlog"
 	"io"
 	"os"
-	"sync"
 	"time"
 )
 
@@ -20,36 +18,24 @@ func NewOutput(filenameSuffix string) (io.WriteCloser, error) {
 }
 
 type output struct {
-	f  *os.File
-	b  bytes.Buffer
-	mu sync.Mutex
+	f *os.File
 }
 
 func (x *output) Close() error {
-	x.mu.Lock()
-	defer x.mu.Unlock()
 	return x.f.Close()
 }
 
 func (x *output) Write(p []byte) (int, error) {
-	x.mu.Lock()
-	defer x.mu.Unlock()
-	x.b.Write(p)
-	if bytes.HasSuffix(p, []byte("\n")) {
-		if err := x.write(); err != nil {
-			log.PrintErr(err, "line", fmt.Sprintf("%q", x.b.String()), structlog.KeyStack, structlog.Auto)
+	if _, err := fmt.Fprint(x.f, time.Now().Format(layoutDatetime), " "); err != nil {
+		return 0, err
+	}
+	if _, err := x.f.Write(p); err != nil {
+		return 0, err
+	}
+	if !bytes.HasSuffix(p, []byte("\n")) {
+		if _, err := x.f.WriteString("\n"); err != nil {
+			return 0, err
 		}
 	}
 	return len(p), nil
-}
-
-func (x *output) write() error {
-	if _, err := fmt.Fprint(x.f, time.Now().Format(layoutDatetime), " "); err != nil {
-		return err
-	}
-	if _, err := x.b.WriteTo(x.f); err != nil {
-		return err
-	}
-	x.b.Reset()
-	return nil
 }
